1ee28132663b56f91582c9133fc2c69690e4397f
bakhtiarmahmoodi73 <bakhtiarmahmoodi1995@gmail.com>
2024-06-06 03:13:17 -0700

    update

---
+++ app.js
@@ -0,0 +1,94 @@
+import getWeatherData from "./utils/httpReq.js";
+import { removeModal, showModal } from "./utils/modal.js";
+import { getWeekDay } from "./utils/customDate.js";
+
+const searchInput = document.querySelector("input");
+const searchButton = document.querySelector("button");
+const weatherContainer = document.getElementById("weather");
+const forecastContainer = document.getElementById("forecast");
+const locationIcon = document.getElementById("location");
+const modalButton = document.getElementById("modal-button");
+
+const renderCurrentWeather = (data) => {
+  if(!data){
+  return;
+  }
+  const weatherJSx = `
+  <h1>${data.name}, ${data.sys.country}</h1>
+  <div id="main">
+  <img alt="weather icon" src="https://openweathermap.org/img/w/${
+    data.weather[0].icon
+  }.png" />
+  <span>${data.weather[0].main}</span>
+  <p>${Math.round(data.main.temp)} °c</P>
+  </div>
+  <div id="info">
+   <p>humidity: <span>${data.main.humidity} %</span></P>
+   <p>wind speed: <span>${data.wind.speed} m/s %</span></P>
+  </div>
+  `;
+  weatherContainer.innerHTML = weatherJSx;
+};
+
+const renderForecastWeather = (data) => {
+  if(!data){
+    return;
+    }
+  forecastContainer.innerHTML = "";
+  data = data.list.filter((obj) => obj.dt_txt.endsWith("12:00:00"));
+  data.forEach((i) => {
+    const forecastJSX = `
+    <div>
+    <img alt="weather icon" src="https://openweathermap.org/img/w/${
+      i.weather[0].icon
+    }.png"/>
+    <h3>${getWeekDay(i.dt)}</h3>
+    <p>${Math.round(i.main.temp)} °c</P>
+    <span>${i.weather[0].main}</span>
+    </div>
+    `;
+    forecastContainer.innerHTML += forecastJSX;
+  });
+};
+const searchHandler = async () => {
+  const cityName = searchInput.value;
+
+  if (!cityName) {
+    showModal("Please enter city name");
+    return;
+  }
+  const currentData = await getWeatherData("current", cityName);
+  renderCurrentWeather(currentData);
+  const forecastData = await getWeatherData("forecast", cityName);
+  renderForecastWeather(forecastData);
+};
+
+const positionCallback = async (position) => {
+  const { latitude, longitude } = position.coords;
+  const currentData = await getWeatherData("current", position.coords);
+  renderCurrentWeather(currentData);
+  const forecastData = await getWeatherData("forecast", position.coords);
+  renderForecastWeather(forecastData);
+};
+const errorCallback = (error) => {
+  showModal(error);
+};
+const locationHandler = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(positionCallback, errorCallback);
+  } else {
+    showModal("your browser does not support geolocation");
+  }
+};
+
+const initHandler =async () => {
+  const currentData = await getWeatherData("current", "sanandaj");
+  renderCurrentWeather(currentData);
+  const forecastData = await getWeatherData("forecast", "sanandaj");
+  renderForecastWeather(forecastData);
+}
+
+searchButton.addEventListener("click", searchHandler);
+locationIcon.addEventListener("click", locationHandler);
+modalButton.addEventListener("click", removeModal);
+document.addEventListener("DOMContentLoaded", initHandler)
